e10357f74f16f5413818c40a63d7fbb543b3bbf3
Cian McKenna <cian.mck20@gmail.com>
2023-12-03 12:23:54 +0000

    Markers Displaying but long and lat problems

--- Assignment/app/src/main/java/com/example/assignment/MapActivity.java
+++ Assignment/app/src/main/java/com/example/assignment/MapActivity.java
@@ -11,6 +11,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -35,8 +36,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private final int FINE_PERMISSION_CODE = 1;
     private GoogleMap myMap;
+    private DarkSkyDatabase darkSkyDatabase;
+
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +51,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         getLastLocation();
 
-        new LoadDarkSkyReservesTask().execute();
     }
 
     private void getLastLocation() {
@@ -70,9 +74,13 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     private void placeMarkers(List<DarkSkyReserve> darkSkyReserves){
+
+        if (myMap == null) {
+            return;
+        }
         for (DarkSkyReserve reserve : darkSkyReserves) {
             LatLng reserveLocation = new LatLng(reserve.getLatitude(), reserve.getLongitude());
-
+            Log.d("Marker_Debug", "Adding marker at Lat: " + reserve.getLatitude() + ", Lng: " + reserve.getLongitude());
             int markerColor = getColorFromString(reserve.getColor());
 
             myMap.addMarker(new MarkerOptions()
@@ -83,14 +91,35 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     private int getColorFromString(String colorString) {
-        return Color.parseColor(colorString);
+        try {
+            // Check if the colorString starts with '#' and has a valid length
+            if (colorString.startsWith("#") && (colorString.length() == 7 || colorString.length() == 9)) {
+                // Parse the colorString as a hexadecimal string
+                int color = (int) Long.parseLong(colorString.substring(1), 16);
+
+                // If the colorString has alpha, remove it
+                if (colorString.length() == 9) {
+                    color &= 0x00FFFFFF;
+                }
+
+                // Convert the color to a hue value in the range [0.0, 360.0)
+                return (int) ((color % 0x01000000) / 16777215.0 * 360.0);
+            } else {
+                // Return a default hue if the colorString is not in the expected format
+                return 0;
+            }
+        } catch (NumberFormatException e) {
+            // Handle the case where parsing fails
+            e.printStackTrace();
+            return 0;
+        }
     }
 
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         myMap = googleMap;
-
+        new LoadDarkSkyReservesTask().execute();
         myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         LatLng loc = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
         myMap.addMarker(new MarkerOptions().position(loc).title("Your Location"));
@@ -114,10 +143,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private class LoadDarkSkyReservesTask extends AsyncTask<Void, Void, List<DarkSkyReserve>> {
         @Override
         protected List<DarkSkyReserve> doInBackground(Void... voids) {
+            Log.d("AsyncTask_Debug", "doInBackground: Executing");
             List<DarkSkyReserve> darkSkyReserves = getDarkSkyReservesFromDatabase();
+            Log.d("AsyncTask_Debug", "doInBackground: Completed, Loaded " + darkSkyReserves.size() + " Dark Sky Reserves");
             if (darkSkyReserves.isEmpty()) {
                 insertDarkSkyReservesFromCSV();
-
                 darkSkyReserves = getDarkSkyReservesFromDatabase();
             }
             return darkSkyReserves;
@@ -126,18 +156,26 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         @Override
         protected void onPostExecute(List<DarkSkyReserve> darkSkyReserves) {
             super.onPostExecute(darkSkyReserves);
+
+            Log.d("AsyncTask_Debug", "Dark Sky Reserves loaded: " + darkSkyReserves.size());
             placeMarkers(darkSkyReserves);
+
+
+            if (darkSkyDatabase != null && darkSkyDatabase.isOpen()) {
+                darkSkyDatabase.close();
+            }
         }
     }
 
     private List<DarkSkyReserve> getDarkSkyReservesFromDatabase(){
-        DarkSkyDatabase skydb = Room.databaseBuilder(getApplicationContext(),
+        DarkSkyDatabase darkSkyDatabase = Room.databaseBuilder(getApplicationContext(),
                 DarkSkyDatabase.class, "darksky-database").build();
 
-        return skydb.darkSkyReserveDao().getALL();
+        return darkSkyDatabase.darkSkyReserveDao().getALL();
     }
 
     private void insertDarkSkyReservesFromCSV() {
+        Log.d("CSV_DEBUG", "Inserting Dark Sky Reserves from CSV");
         try {
             InputStream inputStream = getAssets().open("DarkSkyReserve.csv");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -146,24 +184,31 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] values = line.split(",");
+                try {
+                    String[] values = line.split(",");
 
-                if (values.length == 4) {
-                    String name = values[0];
-                    String color = values[1];
-                    double latitude = Double.parseDouble(values[2]);
-                    double longitude = Double.parseDouble(values[3]);
+                    if (values.length == 4) {
+                        String name = values[0].trim();
+                        String color = values[1].trim();
+                        double latitude = Double.parseDouble(values[2].trim());
+                        double longitude = Double.parseDouble(values[3].trim());
 
-                    DarkSkyReserve reserve = new DarkSkyReserve();
-                    reserve.setName(name);
-                    reserve.setColor(color);
-                    reserve.setLatitude(latitude);
-                    reserve.setLongitude(longitude);
+                        // Log the values for debugging
+                        Log.d("CSV_DEBUG", "Name: " + name + ", Color: " + color +
+                                ", Latitude: " + latitude + ", Longitude: " + longitude);
 
-                    // Insert the data into the database
-                    DarkSkyDatabase darkSkyDatabase = Room.databaseBuilder(getApplicationContext(),
-                            DarkSkyDatabase.class, "darksky-database").build();
-                    darkSkyDatabase.darkSkyReserveDao().insert(reserve);
+                        DarkSkyReserve reserve = new DarkSkyReserve();
+                        reserve.setName(name);
+                        reserve.setColor(color);
+                        reserve.setLatitude(latitude);
+                        reserve.setLongitude(longitude);
+
+                        // Insert the data into the database
+                        darkSkyDatabase.darkSkyReserveDao().insert(reserve);
+                    }
+                } catch (NumberFormatException e) {
+                    // Log the exception
+                    Log.e("CSV_DEBUG", "Error parsing CSV line: " + line, e);
                 }
             }
 
